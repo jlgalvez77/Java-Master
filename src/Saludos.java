9a04064d9532df796fd97ebd008c3435f13820d7
Jose Galvez <jlgalvez77@gmail.com>
2025-01-03 11:08:18 +0100

    Saludo

--- src/Saludos.java
+++ src/Saludos.java
@@ -1,5 +1,7 @@
 public class Saludos {
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Nombre: José Gálvez");
+        System.out.println("Edad: 47 años");
+        System.out.println("Pais: España");
     }
 }
