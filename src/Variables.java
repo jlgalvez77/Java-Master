a7eba99384289612596d27cd99c76a008dd5eeb0
Jose Galvez <jlgalvez77@gmail.com>
2025-01-03 12:17:41 +0100

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+    public static void main(String[] args) {
+        //Variables
+        int age = 30;
+        double salary = 20300.50;
+        String name = "Jose";
+        // Acceder a los valores
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " +salary);
+        System.out.println("Name: " + name);
+
+        // Modificar el valor de las variables
+        age = 35;
+        System.out.println("Age: " + age);
+    }
+}
