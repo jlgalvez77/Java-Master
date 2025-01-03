7f4055b1a398f7715786f605e070c2e207003c9e
Jose Galvez <jlgalvez77@gmail.com>
2025-01-03 10:54:11 +0100

    First commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo {
+    //Funcion main(principal)
+    public static void main(String[] args) {
+        System.out.println("Hola mundo con Java");
+    } // fin metodo main
+}
